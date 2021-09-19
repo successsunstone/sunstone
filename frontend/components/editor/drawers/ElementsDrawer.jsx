4072fa8d6f19ad2536c51acd3e820a96dac158e5
Maysam Torabi <kherad@gmail.com>
2021-09-19 10:08:12 +0430

    adding elements

--- frontend/components/editor/drawers/ElementsDrawer.jsx
+++ frontend/components/editor/drawers/ElementsDrawer.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import DrawerSearch from './DrawerSearch';
+import React, { useState } from 'react';
+// import DrawerSearch from './DrawerSearch';
 import styles from './ElementsDrawer.module.css';
 import scrollbar from './scrollbar.module.css';
 
+export default function (props) {
+  const [bundle, setBundle] = useState(window.bundles[0]);
 
-class ElementsDrawer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { info: false };
-  }
-
-  addElement(shape) {
-    const { addElement } = this.props;
+  const internalAddElement = (shape) => {
+    console.log(shape);
+    const { addElement } = props;
     const element = {
       elementableType: 'Shape',
       transparency: 1,
@@ -24,43 +21,39 @@ class ElementsDrawer extends React.Component {
       },
     };
     addElement(element);
-  }
+  };
 
-  render() {
-    const mockupResponse = [ // need default width height and desc to search
-      { id: 1, shape: 'circle', url: window.circle },
-      { id: 2, shape: 'rectangle', url: window.rectangle },
-      { id: 3, shape: 'triangle', url: window.triangle },
-      { id: 4, shape: 'hexagon', url: window.hexagon },
-      { id: 5, shape: 'pentagon', url: window.pentagon },
-      { id: 6, shape: 'rounded-square', url: window.roundedSquare },
-      { id: 7, shape: 'heart', url: window.heart },
-      { id: 8, shape: 'star', url: window.star },
-      { id: 9, shape: 'right-triangle', url: window.rightTriangle },
-    ];
-    return (
-      <>
-        {/* <DrawerSearch placeholder="Search icons and shapes" /> */}
-        <div className={scrollbar.customScroll}>
-          <div className={styles.elementsDrawer}>
-            <div className={styles.itemList}>
-              {mockupResponse.map((item) => (
-                <div
-                  key={item.id}
-                  className={styles.item}
-                  onClick={() => this.addElement(item.shape)}
-                >
-                  <svg>
-                    <use href={item.url} />
-                  </svg>
-                </div>
-              ))}
-            </div>
+  return (
+    <>
+      {/* <DrawerSearch placeholder="Search icons and shapes" /> */}
+      <div className={scrollbar.horizontalScroll}>
+        {window.bundles.map((thebundle) => (
+          <button
+            key={thebundle.label}
+            type="button"
+            className="bundlebutton"
+            onClick={() => setBundle(thebundle)}>
+            {thebundle.label}
+          </button>
+        ))}
+      </div>
+      <div className={scrollbar.customScroll}>
+        <div className={styles.elementsDrawer}>
+          <div className={styles.itemList}>
+            {bundle.bundle.map((item, index) => (
+              <div
+                key={`${item.name}${index}`}
+                className={styles.item}
+                onClick={() => internalAddElement(item.name)}
+              >
+                <svg>
+                  <use href={item.url} x="0" y="0" width="100" height="100"/>
+                </svg>
+              </div>
+            ))}
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
-
-export default ElementsDrawer;
